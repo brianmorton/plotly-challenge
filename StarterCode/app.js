66f1722ba76b9e4ba56e414a5d33151e2e7ce099
Brian Morton <bmorton925@gmail.com>
2021-04-16 11:41:03 -0400

    partial bar chart

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,10 +1,12 @@
  //import statement
 d3.json("samples.json").then((x) => {
     var names = x.names;
-    console.log(names);
-    //var names = x.names
     var metadata = x.metadata;
-    console.log(metadata);
+    var samples = x.samples;
+
+   // console.log(names);
+   // console.log(metadata);
+   // console.log(samples);
 
     //d3 grab html??
    d3.select("tbody");
@@ -14,30 +16,73 @@ d3.json("samples.json").then((x) => {
 
     //function for checking value from dropdown
     function optionChanged() {
+    d3.event.preventDefault()
     var dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
     var idselected = dropdownMenu.property("value");
     console.log(idselected)
-    index
+    demoindex;
+    samplesindex;
     }
-    var index_pos = 0
+    var index_pos_demo = 0
+    var index_pos_samp = 0
+    var index = 0
+    var index2 = 0
+
+    //index pos check log
+//console.log(metadata[0])
    // demo table info pulled
-   function index (idselected){
+   function demoindex (idselected){
+        d3.event.preventDefault()
         if (metadata.id !== idselected)
-            index_pos +=1
-        else demo_table  
+            index +=1;
+        elseif (metadata.id === idselected)
+        {
+            index_pos_demo = index;
+        };
+    var demo_values = metadata[index_pos] 
+    console.log(demo_values);
+    return demo_values
    };
-   function demo_table (index_pos){
-   var demo_values = metadata[index_pos]
-   console.log(demo_values)
-   return demo_values
-                 }
-
-    });
+   function samplesindex (idselected){
+    d3.event.preventDefault()
+    if (samples.id !== idselected)
+        index2 +=1;
+    elseif(samples.id === idselected)
+    {
+        index_pos_samp = index2;
+    };
+    var samples_values = samples[index_pos_samp]
+    console.log(samples_values);
+    return samples_values
+    };
+});
    
+var testsamples = {"id": "940", "otu_ids": [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342], "sample_values": [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], "otu_labels": ["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"]}
+//horizontal bar chart function
+function barbuild (testsamples) {
+var bar = d3.select("#bar");
+var otu_bar_labels = testsamples.otu_ids;
+var otusorted = otu_bar_labels.reverse()
+var otusliced = otusorted.slice(0, 10);
+console.log(otusliced)
 
-//horizontal bar chart info
+var trace1 = {x: otusliced,
+    y: testsamples.sample_values,
+    text: testsamples.otu_labels,
+    type: "bar"
+}
 
+var databar = trace1;
+
+var layout = {
+    title: "'Bar' Chart",
+
+Plotly.newPlot("bar", databar, layout)
+                }
+}
+barbuild();
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 
 // Use `sample_values` as the values for the bar chart.
 
@@ -46,8 +91,8 @@ d3.json("samples.json").then((x) => {
 // * Use `otu_labels` as the hovertext for the chart.
 
 
-//bubble chart info
-
+//bubble chart function
+var bubble = d3.select("#bubble")
 // * Use `otu_ids` for the x values.
 
 // * Use `sample_values` for the y values.
@@ -59,10 +104,9 @@ d3.json("samples.json").then((x) => {
 // * Use `otu_labels` for the text values.
 
 
-// //function for demographic table creation
-//function demo_build()
+// //function for demographic table creation/key value pairs
 
-// //function for key value pair display?
-
-// //function for horizontal bar chart creation
+//function demo_build(demo_values)
+//{var bar = d3.select("#sample-metadata")
+    
 
